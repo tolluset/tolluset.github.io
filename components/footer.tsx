d5cacf80d2aa6e5a674d0bf114562d7aa9710e3a
tolluset <dlqud19@gmail.com>
2025-04-26 23:47:03 +0900

    feat: add page

--- components/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-white border-t border-gray-200 py-8">
-      <div className="container mx-auto px-4 text-center">
-        <p className="text-gray-600">© {new Date().getFullYear()} 이병현. All rights reserved.</p>
-      </div>
-    </footer>
-  )
-}
